31dea756fe7eb959a1fd6c25593aec7624448446
Softwarica College Lab <softwarica@softwarica.com>
2019-04-23 11:01:26 +0545

    Initial commit

---
+++ app/src/main/java/com/example/fragments/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.fragments;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import Fragments.FirstFragment;
+import Fragments.SecondFragment;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+    private Button btnFragment;
+    private Boolean status = true;
+
+    @Override
+    public void onClick(View v) {
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+
+        if (status) {
+            FirstFragment firstFragment=new FirstFragment();
+            fragmentTransaction.add(R.id.fragmentContainer,firstFragment);
+            fragmentTransaction.commit();
+            btnFragment.setText("Load Second Fragment");
+            status=false;
+        }
+
+        else {
+            SecondFragment secondFragment=new SecondFragment();
+            fragmentTransaction.add(R.id.fragmentContainer,secondFragment);
+            fragmentTransaction.commit();
+            btnFragment.setText("Load first fragment");
+            status=true;
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnFragment.findViewById(R.id.btnFragment);
+        btnFragment.setOnClickListener(this);
+
+
+    }
+}
